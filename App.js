026590f0f26edf22b7e2ad8a00c32ebda09116e1
Henry Amusah <henryamusah@gmail.com>
2025-04-18 15:57:06 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 'use strict';
+import Utilities from "./utils.js";
+// import GameState from "./state.js";
 
 const logo  = document.querySelector('.logo');
 const infoBoard = document.querySelector('.player-turn-info');
@@ -25,14 +27,9 @@ const leftScoreValue = document.querySelector('.x-score-board .x-score-value');
 const rightScoreValue = document.querySelector('.o-score-board .o-score-value');
 const tieScoreValue = document.querySelector('.tied-score-value');
 
-// let gridCells;
+const processor = document.querySelector('.processor');
+const utils = new Utilities();
 
-// let leftScoreCount = Number(leftScoreValue.textContent);
-// let rightScoreCount = Number(rightScoreValue.textContent);
-// let tieScoreCount = Number(tieScoreValue.textContent);
-// leftScoreCount = 0;
-// tieScoreCount = 0;
-// rightScoreCount = 0;
 
 let gameScores = {
   leftScoreCount: Number(leftScoreValue.textContent),
@@ -43,124 +40,189 @@ let gameScores = {
 let p1_symbol = 'O', p2_symbol, cpu_symbol;
 let startSymbol = 'x';
 let vs_cpu, vs_player;
+let origBoard = [...Array(9).keys()];
 
 class Game {
   constructor(){
     playerSelectOption.addEventListener('click', this.togglePlayerLogo.bind(this));
     newGameBtn.addEventListener('click', this.newGame.bind(this));
-    // actionBtn1.addEventListener('click', this.endGame);
-    // actionBtn2.addEventListener('click', () => location.reload());
     restartBtn.addEventListener('click', this.restartGame);
-    //window.addEventListener('DOMContentLoaded', () => this.loadGameState());
 
+    // Detecting page reload and persisting game state on page reload
     let data = window.performance.getEntriesByType('navigation')[0].type;
     if(data === 'reload'){
       this.loadGameState();
       console.log('Page reloaded')
     }
-    console.log(data);
   }
-
-  activatePlayer(bg, playerSymbol){
-    bg.classList.add('bg-toggle');
-    bg.classList.remove('bg-hover');
-    bg.children[0].setAttribute('src', `assets/icon-${playerSymbol}-outline-white.svg`);
-  }
-
-  deactivatePlayer(bg, playerSymbol){
-    bg.classList.remove('bg-toggle');
-    bg.classList.add('bg-hover');
-    bg.children[0].setAttribute('src', `assets/icon-${playerSymbol}-white.svg`);
-  }
-
 
   togglePlayerLogo(e){
     let x_bg = document.querySelector('.x-mark'), o_bg = document.querySelector('.o-mark');
     const { target } = e;
     if(target.classList.contains('x-mark') || target.closest('.x-mark')){
       if(target.classList.contains('bg-hover') || target.closest('.x-mark').classList.contains('bg-hover')){
-        this.activatePlayer(x_bg, 'x'); // Activate selected state for X player
-        this.deactivatePlayer(o_bg, 'o'); // Deactivate selected state for O player
+        utils.activatePlayer(x_bg, 'x'); // Activate selected state for X player
+        utils.deactivatePlayer(o_bg, 'o'); // Deactivate selected state for O player
         p1_symbol = 'X';
       }
     } 
     if(target.classList.contains('o-mark') || target.closest('.o-mark')){
       if(target.classList.contains('bg-hover') || target.closest('.o-mark').classList.contains('bg-hover')){
-        this.activatePlayer(o_bg, 'o'); // Activate selected state for O player
-        this.deactivatePlayer(x_bg, 'x'); // Deactivate selected state for X player
+        utils.activatePlayer(o_bg, 'o'); // Activate selected state for O player
+        utils.deactivatePlayer(x_bg, 'x'); // Deactivate selected state for X player
         p1_symbol = 'O';
       }
     }
-    //console.log(p1);
   }
   
   switchTurn(e){
     const { target } = e;
+    const markDisplay = document.createElement('img');
     if(target.childElementCount === 0 && target.getAttribute('src') === null){
-      const markDisplay = document.createElement('img');
       markDisplay.setAttribute('src', `assets/icon-${startSymbol}.svg`);
       target.append(markDisplay);
-      target.addEventListener('mouseenter', () => {
-        if(target.children[0].getAttribute('src').includes('-o.svg')){
-          target.children[0].setAttribute('src', `assets/icon-o-outline.svg`);
-        } 
-        if(target.children[0].getAttribute('src').includes('-x.svg')){
-          target.children[0].setAttribute('src', `assets/icon-x-outline.svg`);
-        }
-      });
-      target.addEventListener('mouseleave', () => {
-        if(target.children[0].getAttribute('src').includes('-o-outline.svg')){
-          target.children[0].setAttribute('src', `assets/icon-o.svg`)
-        }
-        if(target.children[0].getAttribute('src').includes('-x-outline.svg')){
-          target.children[0].setAttribute('src', `assets/icon-x.svg`);
-        }
-      });
+      origBoard[target.id] = startSymbol;
+      utils.hoverState(target);
       startSymbol = startSymbol === 'x' ? 'o' : 'x';
       infoLogo.setAttribute('src', `assets/icon-${startSymbol}-white.svg`);
     }
-    // if(vs_cpu) this.cpuPlay(cpu_symbol.toLocaleLowerCase());
     target.removeEventListener('click', this.switchTurn);
     this.checkWinner();
   }
 
-  cpuPlay(player){
-    const markDisplay = document.createElement('img');
-    //player = p1_symbol === 'x' ? 'o' : 'x';
-    markDisplay.setAttribute('src', `assets/icon-${player}.svg`);
-    this.bestSpot().append(markDisplay);
-    //infoLogo.setAttribute('src', `assets/icon-${p1_symbol}-white.svg`)
+  vsCpu(){
+    const playerState = this.fetchPlayerState();
+    const gridCells = gridContainer.querySelectorAll('.cell');
+    const cpuSymbol = playerState.cpu_symbol.toLowerCase();
+    const playerSymbol = playerState.p1_symbol.toLowerCase();
+  
+    let cpuTurn = () => {
+      gridCells.forEach((cell) => cell.removeEventListener("click", humanPlay));
+      processor.classList.remove('hidden');
+      const markDisplay = document.createElement('img');
+      setTimeout(() => {
+        markDisplay.setAttribute('src', `assets/icon-${cpuSymbol}.svg`);
+        gridCells[this.bestSpot()].append(markDisplay);
+        utils.hoverState(gridCells[this.bestSpot()]);
+        infoLogo.setAttribute('src', `assets/icon-${playerSymbol}-white.svg`);
+        this.checkWinner();
+        humanPlayerTurn();
+        origBoard.splice(
+        this.bestSpot(), 1,
+        cpuSymbol
+        )
+      }, 3000);  
+    }
+
+    const  humanPlay = (e) => {
+      const playerMarkDisplay = document.createElement("img");
+      const { target } = e;
+      if (
+        target.childElementCount === 0 &&
+        target.getAttribute("src") === null
+      ) {
+        playerMarkDisplay.setAttribute(
+          "src",
+          `assets/icon-${playerSymbol}.svg`
+        );
+        target.append(playerMarkDisplay);
+        utils.hoverState(target);
+        origBoard[target.id] = playerSymbol;
+        infoLogo.setAttribute("src", `assets/icon-${cpuSymbol}-white.svg`);
+        this.checkWinner();
+        // preventing cpu play when win is detected
+        if (popup.classList.contains("hidden")) {
+          cpuTurn();
+        }
+      } 
+    }
+
+    let humanPlayerTurn = () => {
+      processor.classList.add('hidden');
+      gridCells.forEach(cell => cell.addEventListener('click', humanPlay));
+    }
+
+     if(cpuSymbol === 'x'){
+      cpuTurn();
+    } else {
+      humanPlayerTurn();
+    }
+
   }
 
   emptySpot(){
-    return Array.from(gridCells).filter(cell => cell.childElementCount < 1);
+    return origBoard.filter(s => typeof s === 'number');
   }
 
   bestSpot(){
-    return this.emptySpot()[0];
+    const playerState = this.fetchPlayerState()
+    const cpu = playerState.cpu_symbol.toLowerCase();
+    return this.minimax(origBoard, cpu).index;
   }
 
-//   document.addEventListener("DOMContentLoaded", (event) => {
-//   const inGame = loadData()
-//   if(inGame){ // true
-// // fetch array elements and render
-//   }else {
+  minimax(currentBoard, player){
+    const playerState = this.fetchPlayerState()
+    const cpu = playerState.cpu_symbol.toLowerCase();
+    const p1 = playerState.p1_symbol.toLowerCase();
+    let emptyCells = this.emptySpot();
+    
+   if(this.winDetected(currentBoard, p1)){
+    return { score: -10 };
+   } else if(this.winDetected(currentBoard, cpu)){
+    return { score: 10 }
+   } else if(emptyCells.length === 0){
+    return { score: 0 }
+   }
 
-//   }
-//   console.log("DOM fully loaded and parsed");
-// });
+    let allMoves = [];
+    for(let i = 0; i < emptyCells.length; i++){
+      let currentMove = {};
+      currentMove.index = currentBoard[emptyCells[i]];
+      currentBoard[emptyCells[i]] = player;
+
+      if(player === cpu){
+        const result = this.minimax(currentBoard, p1);
+        currentMove.score = result.score;
+      } 
+      else {
+        const result = this.minimax(currentBoard, cpu);
+        currentMove.score = result.score;
+      }
+
+      currentBoard[emptyCells[i]] = currentMove.index;
+      allMoves.push(currentMove);
+    }
+
+    let bestMove;
+    if(player === cpu){
+      let bestScore = -Infinity;
+      for(let i = 0; i < allMoves.length; i++){
+        if(allMoves[i].score > bestScore){
+          bestScore = allMoves[i].score;
+          bestMove = i;
+        }
+      }
+    } else {
+      let bestScore = Infinity;
+      for(let i = 0; i < allMoves.length; i++){
+        if(allMoves[i].score < bestScore){
+          bestScore = allMoves[i].score;
+          bestMove = i;
+        }
+      }
+    }
+    return allMoves[bestMove];
+  }
 
   initGameBoard(){
     const grid = Array(9).fill('').forEach((cell, idx) => {
       const gridCell = document.createElement('div');
       gridCell.classList.add('cell');
       gridCell.id = idx;
-      gridCell.addEventListener('click', this.switchTurn.bind(this));
+      if(vs_player){
+        gridCell.addEventListener('click', this.switchTurn.bind(this));
+      } 
       gridContainer.append(gridCell);
-      //console.log(gridContainer.innerHTML)
-      //console.log(gridCells)
-      // localStorage.setItem('cells', gridContainer.innerHTML);
-      // console.log(gridCell)
     });
   }
 
@@ -183,7 +245,6 @@ class Game {
         console.log('game starts');
       }
     })
-    //location.reload();
   }
 
   endGame(){
@@ -196,7 +257,6 @@ class Game {
     if(target.classList.contains('new-game-vs-cpu')){
       cpu_symbol = p1_symbol === 'X' ? 'O' : 'X';
       if(p1_symbol === 'X'){
-        // localStorage.setItem('p1_symbol', p1_symbol)
         leftPlayerName.textContent = `${p1_symbol} (YOU)`;
         rightPlayerName.textContent = `${cpu_symbol} (CPU)`;
       }else {
@@ -231,7 +291,9 @@ class Game {
 
     this.initGameBoard();
     this.saveGameState();
+    if(vs_cpu){ this.vsCpu() };
 
+    // fetching and rendering game score values
    let { leftScoreCount, tieScoreCount, rightScoreCount } = JSON.parse(localStorage.getItem('gameScores'));
     leftScoreValue.textContent = leftScoreCount;
     tieScoreValue.textContent = tieScoreCount; 
@@ -283,11 +345,7 @@ class Game {
       }
       localStorage.setItem('playerState', JSON.stringify(playerState));
     }
-
     localStorage.setItem('gameScores', JSON.stringify(gameScores));
-    //localStorage.setItem('gridCells', JSON.stringify(gridCells))
-    // localStorage.setItem('cells', gridContainer.innerHTML);
-    
   }
 
   loadGameState(){
@@ -337,14 +395,16 @@ class Game {
 
     if(localStorage.getItem('grid')){
       let grid = localStorage.getItem('grid');
+      let playerState = this.fetchPlayerState();
       
-      //grid.addEventListener('click', this.switchTurn.bind(this))
       gridContainer.innerHTML = grid;
       let gridCells = gridContainer.querySelectorAll('.cell')
-      gridCells.forEach(cell => cell.addEventListener('click', this.switchTurn.bind(this)));
+      if(playerState.vs_player === true){
+        gridCells.forEach(cell => cell.addEventListener('click', this.switchTurn.bind(this)));
+      } else {
+        this.vsCpu();
+      }
     }
-
-    //gridContainer.innerHTML = localStorage.getItem('cells')
   }
 
   fetchGameScore(){
@@ -360,8 +420,7 @@ class Game {
   checkWinner(){
     this.checkRestartBeforeWin();
     const gridCells = document.querySelectorAll('.cell');
-    // let X_win, O_win;
-    // console.log(gridCells[1].children[0].getAttribute('src').includes('icon-x.svg'));
+    let X_win, O_win;
 
     const winCombinations = [
       [0, 1, 2], [3, 4, 5], [6, 7, 8],
@@ -370,11 +429,11 @@ class Game {
     ];
 
     for(let combo of winCombinations){
-      let X_win = combo.every(cell => {
+      X_win = combo.every(cell => {
         return gridCells[cell].children[0]?.getAttribute('src').includes('icon-x.svg');
       });
 
-      let O_win = combo.every(cell => {
+      O_win = combo.every(cell => {
         return gridCells[cell].children[0]?.getAttribute('src').includes('icon-o.svg');
       });
 
@@ -385,14 +444,13 @@ class Game {
           gridCells[cell].style.backgroundColor = '#31C3BD';
           gridCells[cell].children[0].setAttribute('src', 'assets/icon-x-outline-white.svg');
         });
-        console.log(`X wins with ${combo} combinations`);
+        // console.log(`X wins with ${combo} combinations`);
         gridCells.forEach(cellElement => cellElement.replaceWith(cellElement.cloneNode(true)));
 
         let gameScores = this.fetchGameScore();
         // gameScores.leftScoreCount += 1;
         leftScoreValue.textContent = gameScores.leftScoreCount += 1;
         this.updateGameScore(gameScores);
-        console.log(gameScores);
 
         infoLogo.setAttribute('src', 'assets/icon-x-white.svg');
         
@@ -406,13 +464,13 @@ class Game {
             message1.textContent = 'oh no, you lost...';
             winnerIcon.setAttribute('src', 'assets/icon-x.svg');
             message2.style.color = '#31C3BD';
-            console.log(`Oh no, you lost...${cpu_symbol} takes the round or Cpu wins`)
+            // console.log(`Oh no, you lost...${cpu_symbol} takes the round or Cpu wins`)
           } else{
             // you won against cpu with X mark
             message1.textContent = 'you won!';
             winnerIcon.setAttribute('src', 'assets/icon-x.svg');
             message2.style.color = '#31C3BD';
-            console.log(`you won ${p1_symbol} takes the round`)
+            // console.log(`you won ${p1_symbol} takes the round`)
           }
         } else{
           if(p1_symbol === 'X'){
@@ -420,13 +478,13 @@ class Game {
             message1.textContent = 'player 1 wins!';
             winnerIcon.setAttribute('src', 'assets/icon-x.svg');
             message2.style.color = '#31C3BD';
-            console.log(`player 1 wins....${p1_symbol} takes the round`)
+            // console.log(`player 1 wins....${p1_symbol} takes the round`)
           } else{
             // player 2 won against player 1 with X mark
             message1.textContent = 'player 2 wins!';
             winnerIcon.setAttribute('src', 'assets/icon-x.svg');
             message2.style.color = '#31C3BD';
-            console.log(`player 2 wins....${p2_symbol} takes the round`)
+            // console.log(`player 2 wins....${p2_symbol} takes the round`)
           }
         }
         popup.classList.remove('hidden');
@@ -457,13 +515,13 @@ class Game {
             message1.textContent = 'oh no, you lost...';
             winnerIcon.setAttribute('src', 'assets/icon-o.svg');
             message2.style.color = '#F2B137';
-            console.log(`Oh no, you lost...${cpu_symbol} takes the round or Cpu wins`)
+            // console.log(`Oh no, you lost...${cpu_symbol} takes the round or Cpu wins`)
           } else{
             // you won against cpu with O mark
             message1.textContent = 'you won!';
             winnerIcon.setAttribute('src', 'assets/icon-o.svg');
             message2.style.color = '#F2B137';
-            console.log(`you won ${p1_symbol} takes the round`)
+            // console.log(`you won ${p1_symbol} takes the round`)
           }
         } else{
           if(p1_symbol === 'O'){
@@ -471,17 +529,16 @@ class Game {
             message1.textContent = 'player 1 wins!';
             winnerIcon.setAttribute('src', 'assets/icon-o.svg');
             message2.style.color = '#F2B137';
-            console.log(`player 1 wins....${p1_symbol} takes the round`)
+            // console.log(`player 1 wins....${p1_symbol} takes the round`)
           } else{
             // player 2 won against player 1 with O mark
             message1.textContent = 'player 2 wins!';
             winnerIcon.setAttribute('src', 'assets/icon-o.svg');   
             message2.style.color = '#F2B137';
-            console.log(`player 2 wins....${p2_symbol} takes the round`)
+            // console.log(`player 2 wins....${p2_symbol} takes the round`)
           }
         }
         popup.classList.remove('hidden');
-        console.log('O wins');
         return;
       } 
     };
@@ -491,10 +548,11 @@ class Game {
       return cell.childElementCount > 0;
     });
     if(tied){
+      // when all boxes are filled, there's a tie
       gridCells.forEach(cellElement => cellElement.replaceWith(cellElement.cloneNode(true)));
       let gameScores = this.fetchGameScore();
-        tieScoreValue.textContent = gameScores.tieScoreCount += 1;
-        this.updateGameScore(gameScores);
+      tieScoreValue.textContent = gameScores.tieScoreCount += 1;
+      this.updateGameScore(gameScores);
        
         // Display popup
         message1.classList.add('hidden');
@@ -502,7 +560,6 @@ class Game {
         message2.style.color = '#A8BFC9';
         message2.textContent = 'Round tied';
         popup.classList.remove('hidden');
-        console.log('tied score');
     }
 
     popup.addEventListener('click', e => {
@@ -519,8 +576,9 @@ class Game {
 
   checkRestartBeforeWin(){
     /*
-      this function checks whether restart was 
-      detected before a win was detected
+      this function checks whether 
+      the restart button was 
+      clicked before a win was detected
     */
     if(message1.classList.contains('hidden') && winnerIcon.classList.contains('hidden')){
       message1.classList.remove('hidden');
@@ -528,6 +586,43 @@ class Game {
       message2.textContent = 'takes the round';
       actionBtn1.textContent = 'Quit';
       actionBtn2.textContent = 'Next round';
+    }
+  }
+
+  winDetected(board, player){ // will be optimized later
+    if(
+      (board[0] === player &&
+      board[1] === player &&
+      board[2] === player) ||
+
+      (board[3] === player &&
+      board[4] === player &&
+      board[5] === player) ||
+
+      (board[6] === player &&
+      board[7] === player &&
+      board[8] === player) ||
+
+      (board[0] === player &&
+      board[3] === player &&
+      board[6] === player) ||
+
+      (board[1] === player &&
+      board[4] === player &&
+      board[7] === player) ||
+
+      (board[0] === player &&
+      board[4] === player &&
+      board[8] === player) ||
+
+      (board[2] === player &&
+      board[4] === player &&
+      board[6] === player)
+      
+    ){
+      return true;
+    } else {
+      return false;
     }
   }
 }
